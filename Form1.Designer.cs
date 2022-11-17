de1e98c6e231c5caebc4fb070ff165ec22fba1a0
alejandro Fernandez <alejandrofr2212@hotmail.com>
2022-11-17 00:46:19 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Cachina_Fashion_Raul
+{
+    partial class Fro_cachina
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombreproveedor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgProveedores = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnImprime = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDNI_proveedor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProveedores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombreproveedor
+            // 
+            this.txtNombreproveedor.Location = new System.Drawing.Point(104, 50);
+            this.txtNombreproveedor.Name = "txtNombreproveedor";
+            this.txtNombreproveedor.Size = new System.Drawing.Size(219, 20);
+            this.txtNombreproveedor.TabIndex = 0;
+            this.txtNombreproveedor.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "descripcion";
+            this.label2.UseWaitCursor = true;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(92, 98);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(231, 112);
+            this.txtDescripcion.TabIndex = 3;
+            this.txtDescripcion.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Location = new System.Drawing.Point(194, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "INGRESO DE PROVEEDORES";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nombre";
+            this.label4.UseWaitCursor = true;
+            // 
+            // dgProveedores
+            // 
+            this.dgProveedores.AllowUserToOrderColumns = true;
+            this.dgProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProveedores.Location = new System.Drawing.Point(12, 255);
+            this.dgProveedores.Name = "dgProveedores";
+            this.dgProveedores.Size = new System.Drawing.Size(478, 176);
+            this.dgProveedores.TabIndex = 6;
+            this.dgProveedores.UseWaitCursor = true;
+
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnAgregar.Location = new System.Drawing.Point(578, 255);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(90, 41);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.UseWaitCursor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnImprime
+            // 
+            this.btnImprime.BackColor = System.Drawing.Color.LightCoral;
+            this.btnImprime.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnImprime.Location = new System.Drawing.Point(511, 314);
+            this.btnImprime.Name = "btnImprime";
+            this.btnImprime.Size = new System.Drawing.Size(88, 38);
+            this.btnImprime.TabIndex = 8;
+            this.btnImprime.Text = "Imprimir";
+            this.btnImprime.UseVisualStyleBackColor = false;
+            this.btnImprime.UseWaitCursor = true;
+            this.btnImprime.Click += new System.EventHandler(this.btnElimina_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnActualizar.Location = new System.Drawing.Point(563, 387);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(93, 43);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.UseWaitCursor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(399, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "DNI";
+            this.label5.UseWaitCursor = true;
+            // 
+            // txtDNI_proveedor
+            // 
+            this.txtDNI_proveedor.Location = new System.Drawing.Point(447, 89);
+            this.txtDNI_proveedor.Name = "txtDNI_proveedor";
+            this.txtDNI_proveedor.Size = new System.Drawing.Size(152, 20);
+            this.txtDNI_proveedor.TabIndex = 11;
+            this.txtDNI_proveedor.UseWaitCursor = true;
+            // 
+            // Fro_cachina
+            // 
+            this.AcceptButton = this.btnAgregar;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::Cachina_Fashion_Raul.Properties.Resources._425576;
+            this.CancelButton = this.btnImprime;
+            this.ClientSize = new System.Drawing.Size(680, 443);
+            this.Controls.Add(this.txtDNI_proveedor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnImprime);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgProveedores);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreproveedor);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.Name = "Fro_cachina";
+            this.RightToLeftLayout = true;
+            this.Text = "Cachina fashion";
+            this.TopMost = true;
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Fro_cachina_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProveedores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombreproveedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgProveedores;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnImprime;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDNI_proveedor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using PdfSharp.Pdf;
+
+namespace Cachina_Fashion_Raul
+{
+    public partial class Fro_cachina : Form
+    {
+        string cadena = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+        DataTable Proveedor()
+        {
+            SqlConnection cn=new SqlConnection(cadena);
+            SqlDataAdapter da = new SqlDataAdapter("exec usp_Proveedor", cn);
+
+            DataTable tb=new DataTable();
+            da.Fill(tb);
+            return tb;  
+        }
+        public Fro_cachina()
+        {
+            InitializeComponent();
+          
+
+            dgProveedores.DataSource = Proveedor();
+        }
+
+        private void Fro_cachina_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e, DataGridView f)
+        {
+            DataGridViewRow currentRow = dgProveedores.CurrentRow;
+            txtNombreproveedor.Text = f.SelectedCells[0].Value.ToString();
+            txtDNI_proveedor.Text = f.SelectedCells[0].Value.ToString();
+            txtDescripcion.Text = f.SelectedCells[0].Value.ToString();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(cadena);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("usp_inserta_proveedor", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombre", txtNombreproveedor.Text);
+                cmd.Parameters.AddWithValue("@Descripcion", txtDescripcion.Text);
+                cmd.Parameters.AddWithValue("@DNI", txtDNI_proveedor.Text);
+
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show($"Se ha agregado{i} proveedor");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { cn.Close(); }
+            dgProveedores.DataSource = Proveedor();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            SqlConnection cm= new SqlConnection(cadena);
+            try{
+                SqlCommand cmd = new SqlCommand("usp_actualiar_proveedor", cm);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombre", txtNombreproveedor.Text);
+                cmd.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+                cmd.Parameters.AddWithValue("@DNI", txtDNI_proveedor.Text);
+
+                cm.Open();
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show($"se ha actualizado el registro de proveedores{i}proveedor");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {cm.Close(); }
+            dgProveedores = null;   
+        }
+
+        private void btnElimina_Click(object sender, EventArgs e)
+        {
+            /*aqui va el impirmir*/
+         
+        }
+    }
+}
